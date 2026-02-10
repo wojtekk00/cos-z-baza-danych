063a2d0cf22be567b3f90d6116449d1fc43fcc9e
unknown <wojtekrocker12@gmail.com>
2026-02-10 11:05:50 +0100

    utworzenie bazy danych

--- app/src/main/java/com/example/room/TokarkaDAO.java
+++ app/src/main/java/com/example/room/TokarkaDAO.java
@@ -1,4 +1,24 @@
 package com.example.room;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
 public interface TokarkaDAO {
+    @Insert
+    void wstawTokarke(Tokarka tokarka);
+    @Delete
+    void usunTokarke(Tokarka tokarka);
+    @Update
+    void zmienParametryTokarki(Tokarka tokarka);
+    @Query("SELECT * FROM tokareczki")
+    List<Tokarka> zwrocWszystkieTokarki();
+
+    @Query("SELECT model FROM tokareczki WHERE moc_silnika > :moc")
+    List<String> zwrocNazwyTokarekOMocy(int moc);
 }
