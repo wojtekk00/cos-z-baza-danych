adce26707a97813ebcf2463162a5fab85c91a442
unknown <wojtekrocker12@gmail.com>
2026-02-10 11:18:57 +0100

    utworzenie bazy w mainActivity, dodanie rekordow i app inspection

--- app/src/main/java/com/example/room/MaszynyDataBase.java
+++ app/src/main/java/com/example/room/MaszynyDataBase.java
@@ -1,4 +1,26 @@
 package com.example.room;
 
-public class MaszynyDataBase {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Tokarka.class}, version = 1)
+public abstract class MaszynyDataBase extends RoomDatabase {
+    public abstract TokarkaDAO zwrocTokarkaDAO();
+    private static MaszynyDataBase instancja;
+    private static MaszynyDataBase zwrocBazeDanych(Context context){
+        if (instancja == null){
+            instancja = Room.databaseBuilder(
+                    context.getApplicationContext(),
+                    MaszynyDataBase.class,
+                    "maszyny_db")
+                    .allowMainThreadQueries()
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+
+        return instancja;
+    }
 }
