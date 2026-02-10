29a83e7900f2914c2263272f9bf8cf5db2c30850
unknown <wojtekrocker12@gmail.com>
2026-02-10 11:56:16 +0100

    formularz dodawania do bazy

--- app/src/main/java/com/example/room/MainActivity.java
+++ app/src/main/java/com/example/room/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.room;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +12,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     MaszynyDataBase maszynyDB;
+    ListView listView;
+    ArrayAdapter<Tokarka> arrayAdapter;
+    List<Tokarka> tokarkiList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +33,21 @@ public class MainActivity extends AppCompatActivity {
         });
 
         maszynyDB = MaszynyDataBase.zwrocBazeDanych(MainActivity.this);
-        maszynyDB.zwrocTokarkaDAO().wstawTokarke(new Tokarka("Nova", "Nebula", 35, 1500));
-        maszynyDB.zwrocTokarkaDAO().wstawTokarke(new Tokarka("DRHSelmeister", "StratosXL", 55, 2000));
+        //maszynyDB.zwrocTokarkaDAO().wstawTokarke(new Tokarka("Nova", "Nebula", 35, 1500));
+        //maszynyDB.zwrocTokarkaDAO().wstawTokarke(new Tokarka("DRHSelmeister", "StratosXL", 55, 2000));
 
+        listView = findViewById(R.id.listView);
+        tokarkiList = maszynyDB.zwrocTokarkaDAO().zwrocWszystkieTokarki();
+        arrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, tokarkiList);
+        listView.setAdapter(arrayAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                maszynyDB.zwrocTokarkaDAO().usunTokarke(tokarkiList.get(i));
+                tokarkiList.remove(i);
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
     }
 }
